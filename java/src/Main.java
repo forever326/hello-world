8ebaa11379bb9e8c556302d8d91698de2d350439
forever326 <user.email=1449850660@qq.com>
2019-04-02 10:05:32 +0800

    say hello

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] arg)
-	{
+	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
-		System.out.print("1");
+		int a=input.nextInt();
+		String ly=input.next();
+		char ch=ly.charAt(0);
+		for(int b=0;b<a/2;b++)
+		{	
+			for(int c=0;c<a;c++)
+			{
+				System.out.print(ch);
+			}
+			System.out.println();
+			System.out.println();
+		}
 	}
 }
